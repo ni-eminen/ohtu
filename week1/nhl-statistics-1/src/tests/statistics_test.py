8724f9a3b0d3d43e77498dc2f8a27e2fcc3b1239
ni-eminen <matiasnie2@gmail.com>
2022-11-01 21:53:48 +0200

    14-16

---
+++ week1/nhl-statistics-1/src/tests/statistics_test.py
@@ -0,0 +1,39 @@
+import unittest
+
+from player_reader import PlayerReader
+from player import Player
+from statistics import Statistics
+
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+
+class TestStatistics(unittest.TestCase):
+    def setUp(self):
+        # annetaan Statistics-luokan oliolle "stub"-luokan olio
+        self.statistics = Statistics(
+            PlayerReaderStub()
+        )
+
+    def test_search(self):
+        p = self.statistics.search('Semenko')
+        self.assertEqual(p.name, 'Semenko')
+
+        p = self.statistics.search('asdfasfasfdsaf Fogarty')
+        self.assertEqual(p, None)
+
+    def test_top(self):
+        p = self.statistics.top(3)
+        self.assertEqual(p != None, True)
+
+    def test_team(self):
+        team = self.statistics.team('EDM')
+        self.assertEqual(len(team) > 0, True)
