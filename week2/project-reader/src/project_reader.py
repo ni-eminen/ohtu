328c7d2fe7b04b5eff0dab47a9cc9db0759906ba
ni-eminen <matiasnie2@gmail.com>
2022-11-08 15:58:36 +0200

    brawnch

--- week2/project-reader/src/project_reader.py
+++ week2/project-reader/src/project_reader.py
@@ -9,10 +9,7 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        #print(content)
         dic = toml.loads(content)
-        print("-----------------------")
-        print(dic)
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
         return Project(dic["tool"]["poetry"]["name"], dic["tool"]["poetry"]["description"], dic["tool"]["poetry"]["dependencies"], dic["tool"]["poetry"]["dev-dependencies"])
