f7483a45a3f4273199fccc3a99e9e83197856a10
ni-eminen <matiasnie2@gmail.com>
2022-11-25 22:40:41 +0200

    adding two of the same

--- week4/tdd-ostoskori/src/tests/test_ostoskori.py
+++ week4/tdd-ostoskori/src/tests/test_ostoskori.py
@@ -25,3 +25,9 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(self.maito)
         self.kori.lisaa_tuote(self.juusto)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_add_two_of_the_same(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.maito)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
