3ba17124332f55fbab373c563d96772dc7a57f67
ni-eminen <matiasnie2@gmail.com>
2022-11-25 22:39:56 +0200

    adding

--- week4/tdd-ostoskori/src/tests/test_ostoskori.py
+++ week4/tdd-ostoskori/src/tests/test_ostoskori.py
@@ -5,17 +5,23 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
+        self.maito = Tuote("Maito", 3)
+        self.juusto = Tuote("Juusto", 10)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
         self.assertEqual(self.kori.tavaroita_korissa(), 0)
 
     def test_yhden_tuotteen_lisaamisen_jalkeen_korissa_yksi_tavara(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(self.maito)
         self.assertEqual(self.kori.tavaroita_korissa(), 1)
 
     def test_the_price_is_right(self):
-        maito = Tuote("Maito", 3)
-        self.kori.lisaa_tuote(maito)
-        self.assertEqual(self.kori.hinta(), maito.hinta())
+
+        self.kori.lisaa_tuote(self.maito)
+        self.assertEqual(self.kori.hinta(), self.maito.hinta())
+
+    def test_adding_two_basket_has_two(self):
+        self.kori.lisaa_tuote(self.maito)
+        self.kori.lisaa_tuote(self.juusto)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
